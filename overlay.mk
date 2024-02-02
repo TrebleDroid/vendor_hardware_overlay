9f4b8f305a0fa834bbb424e6d6ee59ec40f67e25
Emerald-glitch <84855747+Emerald-glitch@users.noreply.github.com>
2024-02-02 08:28:20 +0000

    Update overlay.mk

--- overlay.mk
+++ overlay.mk
@@ -192,7 +192,6 @@ PRODUCT_PACKAGES += \
 	treble-overlay-realme-9i \
 	treble-overlay-realme-9i-systemui \
 	treble-overlay-realme-c2 \
-	treble-overlay-realme-c25s \
 	treble-overlay-realme-c25s-systemui \
 	treble-overlay-realme-c3 \
 	treble-overlay-realme-gt2master \
