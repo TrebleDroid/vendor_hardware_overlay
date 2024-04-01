5b11e2c338288da5ad9e2232c9e72f16d0b2d260
kaii <70664258+kaii-lb@users.noreply.github.com>
2024-04-01 14:12:32 +0300

    added spark10pro-systemui

--- overlay.mk
+++ overlay.mk
@@ -313,6 +313,7 @@ PRODUCT_PACKAGES += \
 	treble-overlay-tecno-pova5pro \
 	treble-overlay-tecno-pova5pro-systemui \
 	treble-overlay-tecno-spark10pro \
+	treble-overlay-tecno-spark10pro-systemui \
 	treble-overlay-tecno-spark5 \
 	treble-overlay-tecno-spark6 \
 	treble-overlay-telephony-caf-ims \
