6de944e361c8bad3490fad605f06257ad4fdbf02
Fede2782 <78815152+Fede2782@users.noreply.github.com>
2024-07-26 20:27:17 +0200

    Add SystemUI overlay for Galaxy A34 5G
    This fixes rounded corners.

--- overlay.mk
+++ overlay.mk
@@ -239,6 +239,7 @@ PRODUCT_PACKAGES += \
 	treble-overlay-samsung-a33 \
 	treble-overlay-samsung-a33-systemui \
 	treble-overlay-samsung-a34 \
+	treble-overlay-samsung-a34-systemui \
 	treble-overlay-samsung-a40 \
 	treble-overlay-samsung-a40-systemui \
 	treble-overlay-samsung-a50 \
