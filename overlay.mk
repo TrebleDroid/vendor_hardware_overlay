81b3927a23f3ebc8d914eb19bcb779c9041ef96b
hachiman770 <unformed8981@gmail.com>
2024-11-07 04:33:43 +0900

    Add overlay for Samsung A55

--- overlay.mk
+++ overlay.mk
@@ -254,6 +254,8 @@ PRODUCT_PACKAGES += \
 	treble-overlay-samsung-a53-systemui \
 	treble-overlay-samsung-a54x \
 	treble-overlay-samsung-a54x-systemui \
+	treble-overlay-samsung-a55 \
+	treble-overlay-samsung-a55-systemui \
 	treble-overlay-samsung-a60q \
 	treble-overlay-samsung-a7 \
 	treble-overlay-samsung-a70 \
