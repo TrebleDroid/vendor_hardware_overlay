a7f6ccf5f889ddfd4a5ee4027b2f9d746d29a75c
diamantexv01 <115558499+diamantexv01@users.noreply.github.com>
2025-08-15 23:24:12 +0100

    Update overlay.mk

--- overlay.mk
+++ overlay.mk
@@ -436,6 +436,7 @@ PRODUCT_PACKAGES += \
 	treble-overlay-xiaomi-redminote12pro5g \
 	treble-overlay-xiaomi-redminote13pro4g \
 	treble-overlay-xiaomi-redminote13pro4g-systemui \
+	treble-overlay-xiaomi-redminote13pro5g \
 	treble-overlay-xiaomi-redminote5 \
 	treble-overlay-xiaomi-redminote6pro \
 	treble-overlay-xiaomi-redminote6pro-systemui \
